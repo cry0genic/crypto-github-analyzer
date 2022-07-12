bf8c0d764fe7995ccb20b1072cea03374f8c57e7
Aditya Thakur <cry0genic1103@gmail.com>
2022-07-12 14:01:23 +0530

    ADD: ethereum-ecosystems and modified logic for pulling

--- scripts/fetch-ecosystem-repos/index.js
+++ scripts/fetch-ecosystem-repos/index.js
@@ -1,23 +1,60 @@
 require("toml-require").install({ toml: require("toml") });
-const fs = require("fs")
+const fs = require("fs");
+const { Octokit } = require("octokit");
+const octokit = new Octokit({
+    auth: "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"
+});
 
 // input: ecosystem name --> output: list of repos
-const fetchListOfReposForEcosystem = (ecosystemName) => {
+const fetchListOfReposForEcosystem = async (ecosystemName) => {
     const letter = ecosystemName.substring(0, 1);
     const toml = require(`./crypto-ecosystems/data/ecosystems/${letter}/${ecosystemName}`);
     const repos = toml.repo.map((repo) => {
-        return repo.url;
+        return repo.url.split("https://github.com/")[1];
     });
+    for (let index = 0; index < toml.github_organizations.length; index++) {
+        const orgName = toml.github_organizations[index].split("https://github.com/")[1];
+        repos.push(...await fetchOrganizationRepositories(orgName));
+    }
     return repos;
 };
 
-const repoAggregator = (list) => {
+const repoAggregator = async (list) => {
     let resultList = [];
-    list.forEach((ecosystemName) => {
-        resultList.push(...fetchListOfReposForEcosystem(ecosystemName));
-    });
-    fs.writeFileSync("tempResults.json", JSON.stringify(resultList))
+    for (let index = 0; index < list.length; index++) {
+        const ecosys = list[index];
+        resultList.push(...await fetchListOfReposForEcosystem(ecosys));
+    }
+    fs.writeFileSync("tempResults.json", JSON.stringify(resultList));
 };
 
-const list = ["ethereum"];
+const fetchOrganizationRepositories = async (organization) => {
+    let repositories = []
+    try {
+        repositories = await octokit.paginate(`GET /orgs/${organization}/repos`);
+    } catch (error) {
+        console.log(error.message);
+        if (error.message === "Not Found") {
+            return repositories;
+        }
+        throw error
+    }
+    return repositories.map((item) => {
+        return item.full_name;
+    });
+
+};
+
+const list = [
+    "ethereum-2-0",
+    "ethereum-classic",
+    "ethereum-consensus",
+    "ethereum-dev-tools",
+    "ethereum-erush",
+    "ethereum-execution",
+    "ethereum-name-service",
+    "ethereum-vault",
+    "ethereum-virtual-machine",
+    "ethereum"
+];
 repoAggregator(list);
